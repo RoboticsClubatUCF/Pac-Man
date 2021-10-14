501e1f1f7dd7395d8c93925105dd3bb5d55fe7a7
xar-tol <xartol.dev@gmail.com>
2021-10-14 14:23:58 -0400

    adjusts settings
    rm fake old key

--- pacman/pacman/settings.py
+++ pacman/pacman/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vaafs$r(+op_yw1qrh2i(0mpe9!+ow992i^56$!7#)4spvvs1*'
+SECRET_KEY = 'replace-me'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
