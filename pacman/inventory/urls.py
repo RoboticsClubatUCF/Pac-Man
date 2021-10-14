5bcfbc7e0c0c050eaa3e7304f250d6c22e7b9fb6
xar-tol <xartol.dev@gmail.com>
2021-10-14 16:41:10 -0400

    adds admin, location scaffold pages
    adds a scaffold page for admin
    adds a scaffold page for location

--- pacman/inventory/urls.py
+++ pacman/inventory/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name='home'),
+	path('location-map', views.lab_location, name='location')
 ]
--- pacman/inventory/views.py
+++ pacman/inventory/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
 	return HttpResponse("Hello world! Welcome to the inventory home page.")
+
+# Create a view for lab location.
+def lab_location(request):
+	return HttpResponse("Inventory lab location page scaffold.")
