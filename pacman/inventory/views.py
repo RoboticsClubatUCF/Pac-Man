ccf03cba707b0eed1c9c3ba44a6e7f50f7cbe7c7
BiggiePete <peter.cross222@gmail.com>
2021-10-26 17:10:19 -0400

    Add Files

--- pacman/inventory/views.py
+++ pacman/inventory/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template.loader import get_template;
+from .models import Item
 # Create your views here.
 def home(request):
-	return HttpResponse("Hello world! Welcome to the inventory home page.")
+	return render(request,"inventory.html",
+	{})
+
+def search_inventory(request):
+	if request.method == "POST":
+		searched = request.POST['Search'] #returns what they searched
+		#results = Item.objects.all()
+		results = Item.objects.filter(name__contains=searched)
+		num_results = results.count()
+		return render(request,'Search_inventory.html',
+		{'searched' : searched,
+		'results' : results,
+		'num_results': num_results})
+
+
 
 # Create a view for lab location.
 def lab_location(request):
