f485623d8bae9370e22e2f181c9861917181beb4
BiggiePete <81372152+BiggiePete@users.noreply.github.com>
2021-10-26 17:58:56 -0400

    Add files via upload

--- pacman/inventory/models.py
+++ pacman/inventory/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.db.models.base import Model
 # Create your models here.
+class Item(Model):
+    name = models.CharField(max_length=128)
+    quantity = models.IntegerField()
+    description = models.CharField(max_length=256)
+    def __str__(self):
+        return self.name
