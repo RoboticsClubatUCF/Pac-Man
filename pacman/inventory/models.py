705e7950704c4ed6542c3640e49d8a69815cc8cc
BiggiePete <peter.cross222@gmail.com>
2021-11-13 13:18:36 -0500

    Adds Crude Location Mapping, and Updates to Css

--- pacman/inventory/models.py
+++ pacman/inventory/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 from django.db.models.base import Model
 # Create your models here.
+# Do not migrate this model, needs to be updated to new room design and new arch
+#  of how things will be categorized
+# MACRO_LOCATIONS = [
+#     ('T','Table'),
+#     ('R','Rack'),
+#     ('C','Cabinet'),
+#     ('B','WorkBench'),
+# ]
+# SUB_LOCATIONS = [
+#     ('S','Shelf'),
+#     ('U','Underneath'),
+# ]
 
 
 class Item(Model):
@@ -24,16 +36,15 @@ class Item(Model):
         ('Safety Equipment', 'SAFETY EQUIPMENT'),
         ('Power Tool', 'POWER TOOL'),
     ]
-    # will be used later on in the query
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=128)
+    description = models.TextField(max_length=512, blank=True, null=True)
     quantity = models.IntegerField()
-    description = models.CharField(max_length=256, blank=True, null=True)
     general_type = models.CharField(
         blank=True, null=True, max_length=32, choices=GENERAL_TYPES)
+    new_location = models.CharField(max_length=32,blank=True,null=True)
     location = models.CharField(
         max_length=32, choices=GENERAL_LOCATIONS, blank=True, null=True)
     #image = models.ImageField(upload_to ="item_imgs",blank=True,null=True)
-
     def __str__(self):
         return self.name
