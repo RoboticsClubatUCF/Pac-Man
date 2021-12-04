28853c706f9551f4beb1ca28b3e43438c552c156
BiggiePete <peter.cross222@gmail.com>
2021-12-04 17:18:44 -0500

    Cleans Imports Cleans Blank-Space

--- pacman/inventory/models.py
+++ pacman/inventory/models.py
@@ -2,10 +2,7 @@ from django.db import models
 from django.db.models.base import Model
 # Create your models here.
 
-
 # currently suffers from an issue where if there are more than 9 of any item in the room, the rest of the project will not be able to handle it
-
-
 
 def location_id_fix(a):
     if (str(a) == "None" or a == 0):
@@ -56,7 +53,7 @@ class Item(Model):
     general_type = models.CharField(
         blank=True, null=True, max_length=32, choices=GENERAL_TYPES)
     location = models.ForeignKey(Location, on_delete=models.CASCADE, blank=True,null=True, help_text="Location this item can be found at")
-    image = models.CharField(max_length=2048,blank=True,null=True) # hopefully this is long enough to store any img url
+    #image = models.CharField(max_length=2048,blank=True,null=True) # hopefully this is long enough to store any img url
 
     def __str__(self):
         return self.name + "  -  " + str(self.location)
